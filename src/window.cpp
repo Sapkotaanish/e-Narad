7ab847426002cfca94a1fe982d8b01c622bdf305
Kushal Subedi <kushalsubedi2@gmail.com>
2021-07-05 18:52:34 +0545

    Updated menu

--- src/window.cpp
+++ src/window.cpp
@@ -6,6 +6,9 @@ Window::Window(const wxString& title):wxFrame(NULL, wxID_ANY, title){
     WelcomePanel *welcomePanel = new WelcomePanel(this);
 
     menubar = new wxMenuBar;
+    about = new wxMenu;
+    help = new wxMenu;
+    ShareBro = new wxMenu;
 
     file = new wxMenu;
     file->Append(wxID_ANY, wxT("&New"));
@@ -14,15 +17,20 @@ Window::Window(const wxString& title):wxFrame(NULL, wxID_ANY, title){
     file->AppendSeparator();
 
     imp = new wxMenu;
-    imp -> Append(wxID_ANY, wxT("Import newfeed list..."));
-    imp->Append(wxID_ANY, wxT("Import bookmarks..."));
-    imp->Append(wxID_ANY, wxT("Import mail..."));
+    imp -> Append(wxID_ANY, wxT("1"));
+    imp->Append(wxID_ANY, wxT("2"));
+    imp->Append(wxID_ANY, wxT("3"));
 
-    file->AppendSubMenu(imp, wxT("I&mport"));
+    file->AppendSubMenu(imp, wxT("Submenu"));
     quit = new wxMenuItem(file, wxID_EXIT, wxT("&Quit\tCtrl+W"));
-    file->Append(quit);
+    ShareBro->Append(quit);
+    menubar->Append(ShareBro, wxT("&ShareBro"));
     menubar->Append(file, wxT("&File"));
+    menubar->Append(help, wxT("&Help"));
+    menubar->Append(about, wxT("&About"));
+
     Connect(wxID_EXIT, wxEVT_COMMAND_MENU_SELECTED, wxCommandEventHandler(Window::OnQuit));
+    Connect(wxEVT_CLOSE_WINDOW, wxCloseEventHandler(Window::OnClose));
     SetMenuBar(menubar);
     CreateStatusBar();
 };
@@ -30,6 +38,24 @@ Window::Window(const wxString& title):wxFrame(NULL, wxID_ANY, title){
 void Window::OnQuit(wxCommandEvent& WXUNUSED(event)){
     Close(true);
 }
+
+void Window::OnClose(wxCloseEvent& event) 
+{
+  wxMessageDialog *dial = new wxMessageDialog(NULL,
+    wxT("Are you sure to quit?"), wxT("Question"),
+    wxYES_NO | wxNO_DEFAULT | wxICON_QUESTION);
+
+    int ret = dial->ShowModal();
+    dial->Destroy();
+
+    if (ret == wxID_YES) {
+        Destroy();
+    } else {
+        event.Veto();
+    }
+}
+
+
 void Window::setStatus(wxString status){
     SetStatusText(status);
 }
