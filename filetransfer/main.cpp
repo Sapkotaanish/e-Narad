145bf82920a0002b8f60df7b9fa7f3d057d36892
Nabin Khanal <code.nabinkhanal@gmail.com>
2021-07-12 22:08:31 +0545

    messaging started

--- filetransfer/main.cpp
+++ filetransfer/main.cpp
@@ -1,25 +1,33 @@
 #include "Timer.cpp"
-#include "include/Client.hpp"
 #include "include/Download.hpp"
-#include "include/Server.hpp"
+#include "include/FileClient.hpp"
+#include "include/FileServer.hpp"
 #include <iostream>
+#include <thread>
+
+void send() {
+  Server sender(51000);
+  sender.send("/home/nabin/Documents/Advance mathematics.pdf");
+}
+void receive() {
+  Client client(51000);
+  client.receive("check.pdf");
+}
+
 int main() {
-  std::cout << "Enter s for server and c for client." << std::endl;
-  char ans;
-  std::cin >> ans;
-  Timer timer;
-  if (ans == 's') {
-    Server sender(51000);
-    sender.send("/home/nabin/try.cpp");
-  } else {
+  // std::cout << "Enter s for server and c for client." << std::endl;
+  // char ans;
+  // std::cin >> ans;
+  // Timer timer;
+  // std::thread t;
+  // if (ans == 's') {
+  //   t = std::thread(send);
+  // } else {
+  //   t = std::thread(receive);
+  // }
 
-    Client client(51000);
-    client.receive("check.cpp");
-  }
-
-  Download file("http://pixelcaster.com", "/yosemite/webcams/ahwahnee2.jpg",
-                "check.jpg", 80);
-  file.download();
-
+  // Download file("http://74.125.13.83", "/get_video?video_id=qg1ckCkm8YI",
+  // "Check.txt", 80);
+  // file.download();
   return 0;
 }
